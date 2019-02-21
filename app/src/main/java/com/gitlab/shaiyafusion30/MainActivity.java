752b2c68b9ac891b5b8a3e4258ae7960c2a4005d
Server <shaiyafusion30@gmail.com>
2019-02-21 17:21:54 -0400

    Last Message

--- app/src/main/java/com/gitlab/shaiyafusion30/MainActivity.java
+++ app/src/main/java/com/gitlab/shaiyafusion30/MainActivity.java
@@ -3,6 +3,8 @@ package com.gitlab.shaiyafusion30;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.gitlab.shaiyafusion30.dialogpackage.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
